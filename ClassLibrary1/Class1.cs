066948c79027158f5f85c56286eb8273d5b49b67
Scott Musser <scott@heavenword.com>
2019-09-18 12:05:54 -0400

    Project for Clear Measure

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void PrintThemWithLimit(int iUpperLimit, Func<string, int> myPrintFunction)
+        {
+            bool bIgnore;
+            for (int iCntr = 1; iCntr <= iUpperLimit; iCntr++)
+            {
+                bIgnore = false;
+                if (0 == iCntr % 3)
+                {
+                    myPrintFunction("Fizz");
+                    bIgnore = true;
+                }
+                if (0 == iCntr % 5)
+                {
+                    myPrintFunction("Buzz");
+                    bIgnore = true;
+                }
+                if (!bIgnore)
+                    myPrintFunction(iCntr.ToString());
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.PrintThemWithLimit(int.MaxValue, PrintIt);
+        }
+
+        public static int PrintIt(string sValue)
+        {
+            Console.WriteLine(sValue);
+            return 0;
+        }
+    }
+}
